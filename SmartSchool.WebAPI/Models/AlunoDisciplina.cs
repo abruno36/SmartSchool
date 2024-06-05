1701badd12b234655baed12660c6a5716c10669a
= <=>
2024-06-05 08:21:30 -0300

    first commit

---
+++ SmartSchool.WebAPI/Models/AlunoDisciplina.cs
@@ -0,0 +1,17 @@
+﻿namespace SmartSchool.WebAPI.Models
+{
+    public class AlunoDisciplina
+    {
+        public AlunoDisciplina(){}
+
+        public AlunoDisciplina(int aluniId, int disciplinaId)
+        {
+            AlunoId = aluniId;
+            DisciplinaId = disciplinaId;
+        }
+        public int AlunoId { get; set; }
+        public Aluno Aluno { get; set; }
+        public int DisciplinaId { get; set; }
+        public Disciplina Disciplina { get; set; }
+    }
+}
