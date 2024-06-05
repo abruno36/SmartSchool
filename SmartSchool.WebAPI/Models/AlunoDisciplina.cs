5102a8313bb8bfcd0d37efc3a88f93c3eb379883
= <=>
2024-06-05 09:12:35 -0300

    Criação banco MySql

--- SmartSchool.WebAPI/Models/AlunoDisciplina.cs
+++ SmartSchool.WebAPI/Models/AlunoDisciplina.cs
@@ -1,14 +1,18 @@
-﻿namespace SmartSchool.WebAPI.Models
+using System;
+
+namespace SmartSchool.WebAPI.Models
 {
     public class AlunoDisciplina
     {
-        public AlunoDisciplina(){}
-
-        public AlunoDisciplina(int aluniId, int disciplinaId)
+        public AlunoDisciplina() { }
+        public AlunoDisciplina(int alunoId, int disciplinaId)
         {
-            AlunoId = aluniId;
-            DisciplinaId = disciplinaId;
+            this.AlunoId = alunoId;
+            this.DisciplinaId = disciplinaId;
         }
+        public DateTime DataIni { get; set; } = DateTime.Now;
+        public DateTime? DataFim { get; set; }
+        public int? Nota { get; set; } = null;
         public int AlunoId { get; set; }
         public Aluno Aluno { get; set; }
         public int DisciplinaId { get; set; }
--- SmartSchool.WebAPI/Startup.cs
+++ SmartSchool.WebAPI/Startup.cs
@@ -1,14 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using SmartSchool.WebAPI.Data;
 
 namespace SmartSchool.WebAPI
 {
@@ -24,6 +20,11 @@ namespace SmartSchool.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<SmartContext>(
+                context => context.UseMySql(Configuration.GetConnectionString("MySqlConnection"))
+            );
+
             services.AddControllers();
         }
 
